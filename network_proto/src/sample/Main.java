ff6c0e786d6f7ca14a8a480cea0cf50bd3cec182
PaZera <pazera66@gmail.com>
2017-08-12 00:00:02 +0200

    First commit

--- network_proto/src/sample/Main.java
+++ network_proto/src/sample/Main.java
@@ -34,7 +34,7 @@ public class Main extends Application {
     private Server createServer(){
         return new Server(55555, data -> {
             Platform.runLater(() -> {
-                //something
+                //somethingToCommit
             });
         });
     }
