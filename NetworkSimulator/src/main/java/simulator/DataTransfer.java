5fad31f83d9611f2f51622a139c2a1e9052f0d7f
Karol <k.p.krol@stud.elka.pw.edu.pl>
2017-08-20 01:03:52 +0200

    Added main structure of network simulator

---
+++ NetworkSimulator/src/main/java/simulator/DataTransfer.java
@@ -0,0 +1,28 @@
+package simulator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+class DataTransfer {
+    private int sourceNodeId;
+    private int destNodeId;
+    private List<DataPackage> payload;
+    private int dataPackageSize;
+    private int size;
+
+    DataTransfer(int sourceNodeId, int destNodeId, List<DataPackage> payload, int dataPackageSize){
+        this.sourceNodeId = sourceNodeId;
+        this.destNodeId = destNodeId;
+        this.payload = payload;
+        this.dataPackageSize = dataPackageSize;
+        size = calculateSize();
+    }
+
+    private int calculateSize() {
+        return payload.size()*dataPackageSize;
+    }
+}
---
+++ NetworkSimulator/src/main/java/simulator/Network.java
@@ -0,0 +1,32 @@
+package simulator;
+
+import lombok.Getter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Network {
+
+    @Getter
+    private List<DataTransfer> dataTransfers;
+
+    private int dataPackageSize;
+
+    Network(int sourceBitrate){
+        dataTransfers = new LinkedList<>();
+        dataPackageSize = calculateDataPackageSize(sourceBitrate);
+    }
+
+    private int calculateDataPackageSize(int sourceBitrate) {
+        return sourceBitrate / 1000;
+    }
+
+    void addDataTransfer(int source, int dest, List<DataPackage> payload){
+        dataTransfers.add(new DataTransfer(source, dest, payload, dataPackageSize));
+    }
+
+    public void clearNetwork(){
+        dataTransfers.clear();
+    }
+}
