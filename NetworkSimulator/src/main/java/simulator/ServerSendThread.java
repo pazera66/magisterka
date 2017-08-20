2cd5b392e9169a1c543aaa6eb1846b67b75aa9c6
Karol <k.p.krol@stud.elka.pw.edu.pl>
2017-08-20 22:56:12 +0200

    Attempt to multithread server send

---
+++ NetworkSimulator/src/main/java/simulator/ServerSendThread.java
@@ -0,0 +1,26 @@
+package simulator;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ServerSendThread extends Thread {
+
+    private long dataPackagesGenerated;
+    private Network network;
+    private int serverId;
+    private int nodeId;
+
+    ServerSendThread(long dataPackagesGenerated, Network network, int serverId, int nodeId) {
+        this.dataPackagesGenerated = dataPackagesGenerated;
+        this.network = network;
+        this.serverId = serverId;
+        this.nodeId = nodeId;
+    }
+
+    @Override
+    public void run(){
+        List<DataPackage> payload = new LinkedList<>();
+        payload.add(new DataPackage(dataPackagesGenerated));
+        network.addDataTransfer(serverId, nodeId, payload);
+    }
+}
